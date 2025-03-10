d42587ab98e8ef9af50c5aaf6d0d7824ae32c0d4
Kakub Jořínský <jakub.korinsky@student.souepl.cz>
2025-03-10 09:27:06 +0100

    apdejt jak cyp

---
+++ header.php
@@ -0,0 +1,22 @@
+<header>
+    <nav>
+        <a href="/">Stránka 1</a>
+        <a href="/">Stránka 2</a>
+        <a href="/">Stránka 3</a>
+    </nav>
+</header>
+
+<style>
+    header{
+        background: blue;
+        padding: 36px;
+        nav{
+            display: flex;
+            justify-content: flex-end;
+            gap: 36px;
+            a{
+                color: white;
+            }
+        }
+    }
+</style>
--- index.php
+++ index.php
@@ -5,23 +5,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+
+
 <body>
-    <?php
-        $name = "Jméno";
-        $number = 3;
-    ?>
-    <h1><?php echo($name) ?> zde je text.</h1>
 
-    <?php
-        for($i = 0; $i <= 3; $i++){
-            echo "Toto číslo je: $i <br>";
-        }
+    <?php include "header.php" ?>
+    <main>
+        <?php
+            $name = "Jméno";
+            $number = 3;
+        ?>
+        <h1><?php echo($name) ?> zde je text.</h1>
+        <?php
+            for($i = 0; $i <= 3; $i++){
+                echo "Toto číslo je: $i <br>";
+            }
 
-        $j = 0;
-        while($j <= 3){
-            echo "<p>Funguje v cyklu while $j</p>";
-            $j++;
+            $j = 0;
+            while($j <= 3){
+                echo "<p>Funguje v cyklu while $j</p>";
+                $j++;
+            }
+        ?>
+        <?php
+        $user = 'root';
+        $password = 'root';
+        $db = 'prvni_db';
+        $host = 'localhost';
+        $port = 3306;
+
+        $conn = new mysqli($host, $user, $password, $db);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
-    ?>
+        
+        $sql = "SELECT * FROM zbozi";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+
+            while($row = $result->fetch_assoc()) {
+            echo "ID: " . $row["id"]. " - Název produktu: " . $row["nazev produktu"]. " - Cena: " . $row["cena"]. " - Počet kusů: " . $row["pocet kusu"] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
+    </main>
 </body>
 </html>
