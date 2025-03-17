4da73dddff14dde4baf23362178b1813a11f410c
Kakub Jořínský <jakub.korinsky@student.souepl.cz>
2025-03-17 09:25:43 +0100

    vjec

--- index.php
+++ index.php
@@ -54,6 +54,35 @@
         }
         $conn->close();
         ?>
+        <br>
+        <br>
+        <br>
+        <?php
+        $user = 'root';
+        $password = 'root';
+        $db = 'zaci';
+        $host = 'localhost';
+        $port = 3306;
+
+        $conn = new mysqli($host, $user, $password, $db);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        $sql = "SELECT * FROM zak";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+
+            while($row = $result->fetch_assoc()) {
+            echo "ID: " . $row["id"]. " - Jméno: " . $row["Jméno"]. " - Příjmení: " . $row["Příjmení"]. " - Třída: " . $row["Třída"] . " - Věk: " . $row["Věk"] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
     </main>
 </body>
 </html>
